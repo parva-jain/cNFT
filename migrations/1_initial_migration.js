eb41568334d02371dd624f9445dc9814da4acd4b
Parva Jain <parv@Parvas-MacBook-Air.local>
2022-05-20 11:58:01 +0530

    migration script

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,12 @@
 const Migrations = artifacts.require("Migrations");
+const Composable = artifacts.require("ComposableTopDown");
+const SampleNFT = artifacts.require("SampleNFT");
+const SampleERC20 = artifacts.require("SampleERC20");
 
 module.exports = function (deployer) {
   deployer.deploy(Migrations);
+  //deployer.deploy(Composable, "ComposableTopDown", "COMPTD", {gas: 8000000});
+  deployer.deploy(Composable, "ComposableTopDown", "COMPTD");
+  deployer.deploy(SampleNFT, "SampleNFT", "SNFT");
+  deployer.deploy(SampleERC20);
 };
